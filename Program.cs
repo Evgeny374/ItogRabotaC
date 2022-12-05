01f97f2efb2724ed8e588cf83bcf9f872433e8f4
E.Barabanov <barabanov698@gmail.com>
2022-12-05 12:09:15 +0300

    Добавили программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int KolEllements(string[] array)
+{
+int length = array.Length;
+int lengthString = 3;
+int el = 0;
+int i =0;
+    while (i<length)
+    {
+        if (array[i].Length <= lengthString)
+        {
+            el++; i++;
+        } 
+        else i++;   
+    }
+return el;
+}
+
+string[] Mas2(int el)
+{
+string[] array2 = new string [el];
+return array2;
+}
+
+string[] MasNew(string[] array, string[] array2)
+{
+int length = array.Length;
+int lengthString = 3;
+int i =0; int j =0;
+    while (i<length)
+    {
+        if (array[i].Length <= lengthString)
+        {
+            array2[j] = array[i]; i++; j++;   
+        } 
+        else i++;   
+    }
+return array2;
+}
+
+void PrintMas(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+string[] mas1 = {"fdf", "wert", "1", "4567"};
+Console.WriteLine("ИСХОДНЫЙ МАССИВ");
+PrintMas(mas1); 
+//Console.WriteLine("Кол-во эл-тов состоящих из менее 3х символов =" + KolEllements(mas1)); 
+Console.WriteLine("НОВЫЙ МАССИВ");
+PrintMas(MasNew(mas1, Mas2(KolEllements(mas1))));
